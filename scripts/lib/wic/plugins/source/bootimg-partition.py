8d53b801abebfb668943fa185653de13db204619
Paulo Neves <ptsneves@gmail.com>
2018-02-25 16:50:10 +0100

    wic: partition plugin wrongly assumes it is rootfs
    The partition plugin is used as the base for other plugins.
    One of the methods the plugins use, is the prepare_rootfs
    method.
    
    The prepare_rootfs method wrongly assumes that the value
    ROOTFS_SIZE from bitbake datastore is relevant to every
    invocation of prepare_rootfs, which it clearly is not, for
    example in the bootimg-partition case.
    
    This commit adds an optional argument to prepare_rootfs
    where a caller can tell prepare_rootfs if it is an actual
    rootfs and whether related rootfs information retrieved from
    bitbake is valid. The default behavior of this optional
    argument is to assume that the invocation is an actual
    rootfs, to maintain compatibility with previous
    implementations.
    
    Signed-off-by: Paulo Neves <ptsneves@gmail.com>

--- scripts/lib/wic/plugins/source/bootimg-partition.py
+++ scripts/lib/wic/plugins/source/bootimg-partition.py
@@ -120,4 +120,4 @@ class BootimgPartitionPlugin(SourcePlugin):
 
         logger.debug('Prepare boot partition using rootfs in %s', hdddir)
         part.prepare_rootfs(cr_workdir, oe_builddir, hdddir,
-                            native_sysroot)
+                            native_sysroot, False)
